dc85bf15b1503b6761978f7039ab28d60ae5cbd2
wuzhibo <wuzhibo@zzhxamc.com>
2021-06-17 18:07:12 +0800

    init layout

--- rank.js
+++ rank.js
@@ -1,11 +1,11 @@
 export default {
-  xiaoga: 0,
+  xiaoga: -50,
   zhangzong: 0,
-  chenmao: 0,
-  zhongzong: 0,
+  chenmao: -356,
+  zhongzong: -96,
   wudong: 0,
   xiaohei: 0,
   luobo: 0,
   laizong: 0,
-  wu: 0
+  max: 299
 }
